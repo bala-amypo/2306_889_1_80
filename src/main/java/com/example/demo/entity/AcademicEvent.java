f27ca9148b01e68fcfc02dbc14525198c1bc359e
bala-amypo <bala@amypo.in>
2025-12-18 15:25:06 +0000

    User commit

--- src/main/java/com/example/demo/entity/AcademicEvent.java
+++ src/main/java/com/example/demo/entity/AcademicEvent.java
@@ -1,11 +1,108 @@
-public class AcademicEvent{
+package com.example.demo.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+@Entity
+
+public class AcademicEvent {
+
     @Id
-  private long branchd;
-  private String title;
-  private String evenType;
-  private LocalDate startData;
-  private LocalDate ebdData;
-  private String location;
-  private String description;
-  private LocalDateTime submittedAt;
+    @Column(unique = true)
+    private long branchId;
+
+    private String title;
+    private String eventType;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String location;
+    private String description;
+    private LocalDateTime submittedAt;
+
+   
+    public AcademicEvent() {
+    }
+
+ 
+    public AcademicEvent(long branchId, String title, String eventType,
+            LocalDate startDate, LocalDate endDate,
+            String location, String description,
+            LocalDateTime submittedAt) {
+        this.branchId = branchId;
+        this.title = title;
+        this.eventType = eventType;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.location = location;
+        this.description = description;
+        this.submittedAt = submittedAt;
+    }
+
+  
+    public long getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(long branchId) {
+        this.branchId = branchId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getSubmittedAt() {
+        return submittedAt;
+    }
+
+    public void setSubmittedAt(LocalDateTime submittedAt) {
+        this.submittedAt = submittedAt;
+    }
 }
