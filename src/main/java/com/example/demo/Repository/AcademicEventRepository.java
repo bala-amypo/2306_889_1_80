27a126a97426f5089e872558bc2e2ffdc1ab1849
bala-amypo <bala@amypo.in>
2025-12-18 10:15:36 +0000

    User commit

--- src/main/java/com/example/demo/Repository/AcademicEventRepository.java
+++ src/main/java/com/example/demo/Repository/AcademicEventRepository.java
@@ -1,6 +1,9 @@
 package com.example.demo.repository;
-import org.springframework.data.Jpa.repository.JpaRepository;
-import com.example.demo.entity.AcademicEventRepository;
-public interface AcademicEventRepository extends JpaRepository<AcademicEvent,Long>{
-    
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.AcademicEvent;
+
+public interface AcademicEventRepository extends JpaRepository<AcademicEvent, Long> {
+
 }
--- src/main/java/com/example/demo/Repository/ClashRecordRepository.java
+++ src/main/java/com/example/demo/Repository/ClashRecordRepository.java
@@ -1,6 +1,9 @@
 package com.example.demo.repository;
-import org.springframework.data.Jpa.repository.JpaRepository;
-import com.example.demo.entity.ClashRecordRepository;
-public interface ClashRecordRepository extends JpaRepository<ClashRecord,Long>{
-    
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.ClashRecord;
+
+public interface ClashRecordRepository extends JpaRepository<ClashRecord, Long> {
+
 }
