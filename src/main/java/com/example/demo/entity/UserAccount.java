56f1e94632d1d6ff0b5c2bfd5ca5125cfb34170e
bala-amypo <bala@amypo.in>
2025-12-19 04:44:47 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserAccount.java
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -1,10 +1,89 @@
+ package com.example.demo.entity;
 public class UserAccount{
-    @ID
-    @Column(unique=true)
-    private String FullName;
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class UserAccount {
+
+    @Id
+    @Column(unique = true)
+    private String fullName;
+
     private String email;
     private String password;
     private String role;
     private String department;
     private LocalDateTime createdAt;
+
+   
+    public UserAccount() {
+    }
+
+   
+    public UserAccount(String fullName, String email, String password,
+            String role, String department,
+            LocalDateTime createdAt) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.department = department;
+        this.createdAt = createdAt;
+    }
+
+   
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+   
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
+
 }
