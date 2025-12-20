44d5242c4d4279b3662d19dc3de02448e54b9e7f
bala-amypo <bala@amypo.in>
2025-12-20 09:36:57 +0000

    User commit

--- src/main/java/com/example/demo/service/ClashRecordService.java
+++ src/main/java/com/example/demo/service/ClashRecordService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import java.util.List;
 import com.example.demo.entity.ClashRecord;
 
-public interface ClashDetectionService {
+public interface ClashRecordService {
 
     ClashRecord logClash(ClashRecord clash);
 
