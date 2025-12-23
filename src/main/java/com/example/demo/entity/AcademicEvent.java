e750737e6ac2419cef7da24e987190cfa2eb45d4
bala-amypo <bala@amypo.in>
2025-12-23 08:23:56 +0000

    User commit

--- src/main/java/com/example/demo/entity/AcademicEvent.java
+++ src/main/java/com/example/demo/entity/AcademicEvent.java
@@ -30,18 +30,7 @@ public class AcademicEvent {
     public AcademicEvent() {
     }
 
-    public AcademicEvent(Long id, Long branchId, String title, String eventType,
-            LocalDate startDate, LocalDate endDate,
-            String location, String description, LocalDateTime submittedAt) {
-        this.id = id;
-        this.branchId = branchId;
-        this.title = title;
-        this.eventType = eventType;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.location = location;
-        this.description = description;
-      +  this.submittedAt = submittedAt;
+
     }
 
     @PrePersist
