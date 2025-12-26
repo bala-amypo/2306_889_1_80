9b110ad1002f3bca4331d3ec958671961de3f63e
bala-amypo <bala@amypo.in>
2025-12-26 08:14:28 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserAccount.java
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -1,9 +1,9 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "users")
 public class UserAccount {
 
     @Id
@@ -15,32 +15,48 @@ public class UserAccount {
     private String password;
     private String role;
     private String department;
-    private LocalDateTime createdAt;
 
-    public UserAccount() {}
-
-    @PrePersist
-    public void prePersist() {
-        if (createdAt == null) createdAt = LocalDateTime.now();
-        if (role == null) role = "USER";
+    public Long getId() {
+        return id;
     }
 
-   
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public String getFullName() {
+        return fullName;
+    }
 
-    public String getFullName() { return fullName; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() {
+        return email;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
-
-    public String getDepartment() { return department; }
-    public void setDepartment(String department) { this.department = department; }
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+ 
+    public String getRole() {
+        return role;
+    }
+ 
+    public void setRole(String role) {
+        this.role = role;
+    }
+ 
+    public String getDepartment() {
+        return department;
+    }
+ 
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
