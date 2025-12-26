276a2d3a11e4f6244e6271b736cc81aa703a60a8
bala-amypo <bala@amypo.in>
2025-12-26 09:30:05 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserAccount.java
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -1,9 +1,10 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user_accounts")
 public class UserAccount {
 
     @Id
@@ -16,8 +17,40 @@ public class UserAccount {
     private String role;
     private String department;
 
+    private LocalDateTime createdAt;
+
+    public UserAccount() {}
+
+    
+    public UserAccount(Long id, String fullName, String email, String password, String role, String department, LocalDateTime createdAt) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.department = department;
+        this.createdAt = createdAt;
+    }
+
+
+    @PrePersist
+    public void prePersist() {
+        if (createdAt == null) {
+            createdAt = LocalDateTime.now();
+        }
+        if (role == null) {
+            role = "REVIEWER";
+        }
+    }
+
+   
     public Long getId() {
         return id;
+    }
+
+   
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFullName() {
@@ -39,24 +72,33 @@ public class UserAccount {
     public String getPassword() {
         return password;
     }
- 
+
     public void setPassword(String password) {
         this.password = password;
     }
- 
+
     public String getRole() {
         return role;
     }
- 
+
     public void setRole(String role) {
         this.role = role;
     }
- 
+
     public String getDepartment() {
         return department;
     }
- 
+
     public void setDepartment(String department) {
         this.department = department;
+    }
+
+    
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 }
--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -1,15 +1,15 @@
 package com.example.demo.servlet;
 
-import jakarta.servlet.annotation.WebServlet; 
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 import java.io.IOException;
-
 
 @WebServlet(urlPatterns = "/simple-status")
 public class SimpleStatusServlet extends HttpServlet {
 
+    
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
         resp.setStatus(200);
         resp.getWriter().write("Servlet Alive");
