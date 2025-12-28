290cbdda0d2770e17ffbf27ce8e16f8468e1ab19
bala-amypo <bala@amypo.in>
2025-12-28 13:28:17 +0000

    User commit

--- src/main/java/com/example/demo/dto/ApiResponse.java
+++ src/main/java/com/example/demo/dto/ApiResponse.java
@@ -10,7 +10,7 @@ public class ApiResponse {
     public ApiResponse(boolean success, String message) {
         this.success = success; this.message = message;
     }
-    // Getters Setters
+   
     public boolean isSuccess() { return success; }
     public void setSuccess(boolean success) { this.success = success; }
     public String getMessage() { return message; }
